786f30f3fbfc35ae630ee39dbb1692507316424f
Chris Farrant <christopherfarrant1998@gmail.com>
2020-07-01 11:59:31 -0600

    Fleshed Out Vector2D

--- Engine/Math/Vector2D.h
+++ Engine/Math/Vector2D.h
@@ -8,6 +8,7 @@ namespace nc
 
 		Vector2D() : x{ 0 }, y{ 0 } {}
 		Vector2D(float x, float y) : x{ x }, y{ y } {}
+		Vector2D(int x, int y) : x{ static_cast<float>(x) }, y{ static_cast<float>(y) } {}
 
 		void Set(float x, float y) { this->x = x; this->y = y; }
 
@@ -20,5 +21,58 @@ namespace nc
 		Vector2D operator - (float s) const { return Vector2D{ x - s, y - s }; }
 		Vector2D operator * (float s) const { return Vector2D{ x * s, y * s }; }
 		Vector2D operator / (float s) const { return Vector2D{ x / s, y / s }; }
+
+		Vector2D& operator += (const Vector2D& v) { x += v.x; y += v.y; return *this; }
+		Vector2D& operator -= (const Vector2D& v) { x -= v.x; y -= v.y; return *this; }
+		Vector2D& operator *= (const Vector2D& v) { x *= v.x; y *= v.y; return *this; }
+		Vector2D& operator /= (const Vector2D& v) { x /= v.x; y /= v.y; return *this; }
+
+		Vector2D& operator += (float s) { x += s; y += s; return *this; }
+		Vector2D& operator -= (float s) { x -= s; y -= s; return *this; }
+		Vector2D& operator *= (float s) { x *= s; y *= s; return *this; }
+		Vector2D& operator /= (float s) { x /= s; y /= s; return *this; }
+
+		float Length() const;
+		float LengthSquared() const;
+		
+		static float Distance(const Vector2D& v1, const Vector2D& v2);
+		Vector2D Normalized() const;
+		void Normalize();
 	};
+
+	float Vector2D::Length() const
+	{
+		return std::sqrt((x * x) + (y * y));
+	}
+
+	float Vector2D::LengthSquared() const
+	{
+		return (x * x) + (y * y);
+	}
+
+	float Vector2D::Distance(const Vector2D& v1, const Vector2D& v2)
+	{
+		Vector2D v = v1 - v2;
+		return v.Length();
+	}
+	
+	Vector2D Vector2D::Normalized() const
+	{
+		float length = Length();
+		Vector2D n = (length == 0.0f) ? Vector2D{ 0.0f, 0.0f } : *this / length;
+		return n;
+	}
+	
+	void Vector2D::Normalize()
+	{
+		float length = Length();
+		if (length == 0.0f)
+		{
+			*this = Vector2D{ 0.0f, 0.0f };
+		}
+		else
+		{
+			*this /= length;
+		}
+	}
 }
--- Game/Game.cpp
+++ Game/Game.cpp
@@ -7,30 +7,51 @@
 #include "Math/Vector2D.h"
 #include <iostream>
 
-nc::Vector2D position{ 400, 300 };
+const size_t NUM_POINTS = 40;
+float speed = 5.0;
 std::vector<nc::Vector2D> points;
+nc::Vector2D position{ 400, 300 };
 
 bool Update(float dt)
 {
-	return false;
+	bool quit = Core::Input::IsPressed(Core::Input::KEY_ESCAPE);
+	
+	//if (Core::Input::IsPressed(Core::Input::KEY_LEFT) || Core::Input::IsPressed('A')) { position += nc::Vector2D{ -1.0f, 0.0f } * speed; }
+	//if (Core::Input::IsPressed(Core::Input::KEY_RIGHT) || Core::Input::IsPressed('D')) { position += nc::Vector2D{ 1.0f, 0.0f } * speed; }
+	//if (Core::Input::IsPressed(Core::Input::KEY_DOWN) || Core::Input::IsPressed('S')) { position += nc::Vector2D{ 0.0f, 1.0f } * speed; }
+	//if (Core::Input::IsPressed(Core::Input::KEY_UP) || Core::Input::IsPressed('W')) { position += nc::Vector2D{ 0.0f, -1.0f } * speed; }
+
+	int x, y;
+	Core::Input::GetMousePos(x, y);
+
+	nc::Vector2D target = nc::Vector2D{ x,y };
+	nc::Vector2D direction = target - position; // (head <- tail)
+
+	position = position + direction.Normalized() * 5.0f;
+
+	for (nc::Vector2D& point : points) {
+		point = nc::Vector2D{ nc::Random(-10.0f, 10.0f), nc::Random(-10.0f, 10.0f) };
+	}
+
+	return quit;
 }
 
 void Draw(Core::Graphics& graphics)
 {
 	graphics.SetColor(RGB(rand() % 256, rand() % 256, rand() % 256));
-	graphics.DrawLine(static_cast<float>(rand() % 800), static_cast<float>(rand() % 600), static_cast<float>(rand() % 800), static_cast<float>(rand() % 600));
+	//graphics.DrawLine(static_cast<float>(rand() % 800), static_cast<float>(rand() % 600), static_cast<float>(rand() % 800), static_cast<float>(rand() % 600));
 
-	for (size_t i = 0; i < 40 - 1; i++)
+	for (size_t i = 0; i < NUM_POINTS - 1; i++)
 	{
-		nc::Vector2D p1 = points[i];
-		nc::Vector2D p2 = points[i + 1];
+		nc::Vector2D p1 = position + points[i];
+		nc::Vector2D p2 = position + points[i + 1];
 		graphics.DrawLine(p1.x, p1.y, p2.x, p2.y);
 	}
 }
 
 int main()
 {
-	for (size_t i = 0; i < 40; i++)
+	for (size_t i = 0; i < NUM_POINTS; i++)
 	{
 		points.push_back(nc::Vector2D{ nc::Random(0.0f, 800.0f), nc::Random(0.0f, 600.0f) });
 	}
